6890dcb0305e96d3ce95ca2f854f4a4101ccf5c1
Kapparin <116474667+Kapparina@users.noreply.github.com>
2024-03-04 00:36:45 +1100

    chore: Add various dot files

---
+++ .config/fish/config.fish
@@ -0,0 +1,62 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+# Clear and list contents with detailed information
+function ca
+    clear
+    ls -lah --color=auto --full-time
+end
+
+# Count the number of files in the current directory
+function countFiles
+    ls -1 | wc -l
+end
+
+# List contents with detailed information
+alias ll="ls -lah"
+
+# List contents with color coding and file type indicators
+alias ls="ls -F -1 --color=auto --show-control-chars"
+
+# Source config after changes
+alias spro="source ~/.config/fish/config.fish"
+
+# Custom cd + ls function
+function cdls
+    if test -z $argv
+        echo "Usage: cdls <directory>"
+        return 1
+    end
+
+    if test -d $argv[1]
+        cd $argv[1]
+    else
+        echo "Directory not found: $argv[1]"
+        return 1
+    end
+
+    ls
+end 
+
+# Edit config.fish
+function confish
+    nvim "~/.config/fish/config.fish"
+end
+
+# Equivalent to cdls but with zoxide
+function zl
+    if test -z $argv
+        echo "Usage: zl <directory>"
+        return 1
+    end
+
+    z $argv[1]
+
+    ls
+end
+
+starship init fish | source
+zoxide init fish | source
+
+set -g fish_key_bindings fish_vi_key_bindings
